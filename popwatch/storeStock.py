afbab72793dee357e22060edd0cf4306e2f1133a
Stephen Klaus <klaus385385@icloud.com>
2019-03-15 13:21:14 -0700

    Used better method to get chromedriver

--- popwatch/storeStock.py
+++ popwatch/storeStock.py
@@ -17,6 +17,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
 _LOG = logging.getLogger(__name__)
 
@@ -45,8 +46,10 @@ class storeStock(object):
         chrome_options.add_argument("--credentials_enable_service=false")
         chrome_options.add_argument("--profile.password_manager_enabled=false")
 
-        driver = webdriver.Chrome(executable_path=config.DRIVER_LOCATION,
+        driver = webdriver.Chrome(ChromeDriverManager().install(),
                                   chrome_options=chrome_options)
+        # driver = webdriver.Chrome(executable_path=config.DRIVER_LOCATION,
+        #                           chrome_options=chrome_options)
         driver.wait = WebDriverWait(driver, 3)
 
         _LOG.info('Initialized chrome driver.')
