aca3b4b68d7cf6f9977a6f2c046f90f4e5b1eb2d
Stephen Klaus <klaus385385@me.com>
2019-03-15 12:42:22 -0700

    Adding Debian/Distro Checking...

---
+++ init-monitor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source Funko_Pop_Watcher/bin/activate
+export TELEGRAM_TOKEN="653625270:AAE3SY6TQp7eV_XdwjT1eeLqCRGWMX9HR0s"
+export TELEGRAM_CHATID="482379749"
+python ./pop_bot.py
+
