0a4ce39ad38b0527a88f1f044222045bb7414145
Stephen Klaus <klaus385385@icloud.com>
2019-03-28 10:36:20 -0700

    Added initial code for profiles.

--- popwatch/storeStock.py
+++ popwatch/storeStock.py
@@ -8,7 +8,8 @@ import portalocker
 import distro
 
 from popwatch import config
-from popwatch import hotboxCheckout
+#from popwatch import hotboxCheckout
+from popwatch import popProfiles
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
@@ -22,23 +23,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from operator import itemgetter
 
-_LOG = logging.getLogger(__name__)
+# Get Profile From Json Config
+def profileInit():
+    with portalocker.Lock(config.USER_INFO, "r", timeout=10) as data_file:
+        userProfileVars = json.load(data_file)
+        city = userProfileVars.get('city')
+        ccNumber = userProfileVars.get('ccNumber')
+        f_name = userProfileVars.get('f_name')
+        l_name = userProfileVars.get('l_name')
+        zipCode = userProfileVars.get('zipCode')
+        mm = userProfileVars.get('mm')
+        yy = userProfileVars.get('yy')
+        phone = userProfileVars.get('phone')
+        ad_one = userProfileVars.get('ad_one')
+        ad_two = userProfileVars.get('ad_two')
+        ccSecurityCode = userProfileVars.get('ccSecurityCode')
+        email = userProfileVars.get('email')
+        ccOwner = f_name + " " + l_name
+        expDate = mm + '/' + yy
 
-# User Defined Variables
-email = "testemail1@gmail.com"
-f_name = "Test"
-l_name = "Email"
-zipCode = "85306"
-ad_one = "16239 N 45th Dr."
-ad_two = ""
-city = "Glendale"
-phone = "4807682764"
-ccOwner = f_name + " " + l_name
-ccNumber = "4342561969894295"
-mm = "08"
-yy = "22"
-expDate = mm + '/' + yy
-ccSecurityCode = "498"
+    return popProfiles.Profile(city, ccNumber, f_name, l_name, zipCode, phone, ad_one, ad_two, ccSecurityCode, email, ccOwner, expDate)
+
+# Instantiating Pop Profile
+profile = profileInit()
+
+_LOG = logging.getLogger(__name__)
 
 HTML_OBJ = {
     "hottopic": "presale-pdp",
@@ -67,54 +76,56 @@ class storeStock(object):
     def ht_bl_checkout_process(self):
         # Checkout Button then as unregistrated user
         checkoutBtn = WebDriverWait(self.driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="checkout-form"]/fieldset/div/button'))).click();
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="checkout-form"]/fieldset/div/button')))
+        checkoutBtn.click()
         # Start as an Unregistered User
         checkoutBtnAsGuest = WebDriverWait(self.driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="primary"]/div[2]/div[1]/form/fieldset/div/button/span'))).click();
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="primary"]/div[2]/div[1]/form/fieldset/div/button/span')))
+        checkoutBtnAsGuest.click()
         # Fill Out Form for Guest Checkout
         # USER FORM AUTOMATION #
         email_form = self.driver.find_element_by_id("dwfrm_singleshipping_email_emailAddress")
-        email_form.send_keys(email)
-        first_name_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_firstName")
-        first_name_form.send_keys(f_name)
-        last_name_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_lastName")
-        last_name_form.send_keys(l_name)
-        country_selection =  WebDriverWait(self.driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="dwfrm_singleshipping_shippingAddress_addressFields_country"]/option[2]'))).click();
-        zip_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_postal")
-        zip_form.send_keys(zipCode)
-        ad_one_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_address1")
-        ad_one_form.send_keys(ad_one)
-        ad_two_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_address2")
-        ad_two_form.send_keys(ad_two)
-        city_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_city")
-        city_form.send_keys(city)
-        state_selection =  WebDriverWait(self.driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="dwfrm_singleshipping_shippingAddress_addressFields_states_state"]/option[5]'))).click();
-        phone_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_phone")
-        phone_form.send_keys(phone)
-        # Continue to Billing Button
-        continueBillingBtn = WebDriverWait(self.driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="dwfrm_singleshipping_shippingAddress"]/div[2]/fieldset/div/button'))).click();
-        # Enter Credit Card Information
-        # Credit Card Owner
-        creditCardOwner = self.driver.find_element_by_id("dwfrm_billing_paymentMethods_creditCard_owner")
-        creditCardOwner.send_keys(ccOwner)
-        # Credit Card Number
-        creditCardNum = self.driver.find_element_by_id("dwfrm_billing_paymentMethods_creditCard_number")
-        creditCardNum.send_keys(ccNumber)
-        # Credit Card Expiration Date
-        creditCardExp = self.driver.find_element_by_id("dwfrm_billing_paymentMethods_creditCard_userexp")
-        creditCardExp.send_keys(expDate)
-        # Credit Card Security Code
-        creditCardCCV = self.driver.find_element_by_id("dwfrm_billing_paymentMethods_creditCard_cvn")
-        creditCardCCV.send_keys(ccSecurityCode)
-        # Billing Review Button
-        reviewBillingBtn = WebDriverWait(self.driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="dwfrm_billing"]/div[3]/button'))).click();
-        # Place Order Button
-        placeOrderBtn = WebDriverWait(self.driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="summarySubmit"]'))).click();
+        email_form.send_keys(profile.email)
+        # first_name_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_firstName")
+        # first_name_form.send_keys(f_name)
+        # last_name_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_lastName")
+        # last_name_form.send_keys(l_name)
+        # country_selection =  WebDriverWait(self.driver, 20).until(
+        # EC.element_to_be_clickable((By.XPATH, '//*[@id="dwfrm_singleshipping_shippingAddress_addressFields_country"]/option[2]'))).click();
+        # zip_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_postal")
+        # zip_form.send_keys(zipCode)
+        # ad_one_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_address1")
+        # ad_one_form.send_keys(ad_one)
+        # ad_two_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_address2")
+        # ad_two_form.send_keys(ad_two)
+        # city_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_city")
+        # city_form.send_keys(city)
+        # state_selection =  WebDriverWait(self.driver, 20).until(
+        # EC.element_to_be_clickable((By.XPATH, '//*[@id="dwfrm_singleshipping_shippingAddress_addressFields_states_state"]/option[5]'))).click();
+        # phone_form = self.driver.find_element_by_id("dwfrm_singleshipping_shippingAddress_addressFields_phone")
+        # phone_form.send_keys(phone)
+        # # Continue to Billing Button
+        # continueBillingBtn = WebDriverWait(self.driver, 20).until(
+        # EC.element_to_be_clickable((By.XPATH, '//*[@id="dwfrm_singleshipping_shippingAddress"]/div[2]/fieldset/div/button'))).click();
+        # # Enter Credit Card Information
+        # # Credit Card Owner
+        # creditCardOwner = self.driver.find_element_by_id("dwfrm_billing_paymentMethods_creditCard_owner")
+        # creditCardOwner.send_keys(ccOwner)
+        # # Credit Card Number
+        # creditCardNum = self.driver.find_element_by_id("dwfrm_billing_paymentMethods_creditCard_number")
+        # creditCardNum.send_keys(ccNumber)
+        # # Credit Card Expiration Date
+        # creditCardExp = self.driver.find_element_by_id("dwfrm_billing_paymentMethods_creditCard_userexp")
+        # creditCardExp.send_keys(expDate)
+        # # Credit Card Security Code
+        # creditCardCCV = self.driver.find_element_by_id("dwfrm_billing_paymentMethods_creditCard_cvn")
+        # creditCardCCV.send_keys(ccSecurityCode)
+        # # Billing Review Button
+        # reviewBillingBtn = WebDriverWait(self.driver, 20).until(
+        # EC.element_to_be_clickable((By.XPATH, '//*[@id="dwfrm_billing"]/div[3]/button'))).click();
+        # # Place Order Button
+        # placeOrderBtn = WebDriverWait(self.driver, 20).until(
+        # EC.element_to_be_clickable((By.XPATH, '//*[@id="summarySubmit"]'))).click();
 
     def init_driver(self):
         chrome_options = Options()
